6400ecb8cbc07766105cf4ab3b3a4b158f9f8ade
jpl33 <pladott1@gmail.com>
2014-09-21 01:39:53 +0300

    some comments in cahematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -25,6 +25,7 @@ cacheSolve <- function(x, ...) {
   inv <- x$getinv()
   if(!is.null(inv)) {
     message("getting cached data")
+    #return the cached inverse matrix
     return(inv)
   }
   data <- x$get()
